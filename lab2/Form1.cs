268b65210b3b1fc89bc509fbe00dca32cab565fc
Noor Ul Huda <nh958829@gmail.com>
2024-01-28 14:09:40 +0500

    Crud operation of Student

--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using Configuration
 namespace crudUi
 {
     public partial class Form1 : Form
@@ -15,6 +16,18 @@ namespace crudUi
         public Form1()
         {
             InitializeComponent();
+            var con = Configuration.getInstance().getConnection();
+            SqlCommand cmd = new SqlCommand("Select * from Student", con);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
         }
     }
 }
