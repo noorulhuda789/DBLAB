3f3bb11b53e46baa1ec200a461eb5d29d2438c74
Noor Ul Huda <nh958829@gmail.com>
2024-02-12 00:43:44 +0500

    lab 4 completed

--- Lab4/Query.sql
+++ Lab4/Query.sql
@@ -2,10 +2,12 @@ SELECT ProductName
 FROM Products
 WHERE UnitPrice > (SELECT AVG(UnitPrice) FROM Products);
 
-select ShippedDate,Count(*) As numberoforders
-From Orders
-Group By ShippedDate
-Order By ShippedDate;
+SELECT ShippedDate, COUNT(*) AS NumberOfOrders
+FROM Orders
+WHERE ShippedDate IS NOT NULL
+GROUP BY ShippedDate
+ORDER BY ShippedDate;
+
 
 
 Select Country 
@@ -23,14 +25,14 @@ Order By Month(ShippedDate);
 
 Select o.OrderID,Sum(Discount) AS Discount
 From [Order Details] o
-Where Discount>0
 GRoup By  OrderID
+HAVING SUM(Discount) > 0
 Order By o.OrderID;
 
 
 Select ShipCity,Count(*) AS  Numberoforders
 From Orders
-Where ShipCountry='USA'
+HAVING ShipCountry='USA'
 AND Year(ShippedDate)=1997
 GRoup By ShipCity;
 
@@ -40,11 +42,10 @@ Where RequiredDate<ShippedDate
 Group By ShipCountry
  
 
-Select o.OrderID,Sum(Discount) AS Discount,Sum((UnitPrice*Quantity)+((UnitPrice*Quantity)/Discount) ) AS TotalPrice
-From [Order Details] o
-Where Discount>0
-GRoup By  OrderID
-Order By o.OrderID;
+SELECT OrderID, SUM(Discount) AS Discount, SUM(UnitPrice* Quantity) AS TotalPrice
+FROM [Order Details]
+GROUP BY OrderID
+HAVING SUM(Discount) > 0;
 
 Select ShipRegion, ShipCity,Count(*) AS  Numberoforders
 From Orders
